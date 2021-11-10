161c2ffdd1cd7506867de8ae15886a6d263811b3
Alex Gleason <alex@alexgleason.me>
2021-11-10 14:55:01 -0600

    mix new ueberauth_mastodon

---
+++ lib/ueberauth_mastodon.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthMastodon do
+  @moduledoc """
+  Documentation for `UeberauthMastodon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthMastodon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_mastodon_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthMastodonTest do
+  use ExUnit.Case
+  doctest UeberauthMastodon
+
+  test "greets the world" do
+    assert UeberauthMastodon.hello() == :world
+  end
+end
