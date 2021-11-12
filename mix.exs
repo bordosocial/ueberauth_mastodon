b2da5bee448a9417dd24c0331bf0960f85072759
Alex Gleason <alex@alexgleason.me>
2021-11-12 00:06:10 -0600

    Add Hex metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -6,8 +6,33 @@ defmodule UeberauthMastodon.MixProject do
       app: :ueberauth_mastodon,
       version: "0.1.0",
       elixir: "~> 1.11",
+      name: "ueberauth_mastodon",
+      description: "Überauth strategy for Mastodon and Pleroma.",
+      source_url: "https://gitlab.com/soapbox-pub/ueberauth_mastodon",
+      package: package(),
+      docs: docs(),
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitLab" => "https://gitlab.com/soapbox-pub/ueberauth_mastodon"}
+    ]
+  end
+
+  def docs do
+    [
+      main: "readme",
+      extras: [
+        "README.md",
+        "LICENSE.md"
+      ],
+      groups_for_modules: groups_for_modules(),
+      source_ref: "v0.1.0"
     ]
   end
 
@@ -18,6 +43,10 @@ defmodule UeberauthMastodon.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -27,5 +56,9 @@ defmodule UeberauthMastodon.MixProject do
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.24.2", only: :dev, runtime: false}
     ]
+  end
+
+  defp groups_for_modules do
+    []
   end
 end
