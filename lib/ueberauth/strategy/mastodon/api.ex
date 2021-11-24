02c30f0a6496e3228c84cf02963ab4e726aafb3f
Alex Gleason <alex@alexgleason.me>
2021-11-23 20:08:36 -0600

    Use mastodon_client, let it set the Tesla dep

--- lib/ueberauth/strategy/mastodon/api.ex
+++ lib/ueberauth/strategy/mastodon/api.ex
@@ -1,25 +1,17 @@
 defmodule Ueberauth.Strategy.Mastodon.API do
-  use Tesla
-  plug(Tesla.Middleware.JSON)
-
   def app_create(base_url, params) do
-    base_url
-    |> build_url("/api/v1/apps")
-    |> post(params)
+    conn = %MastodonClient.Conn{instance: base_url}
+    MastodonClient.post(conn, "/api/v1/apps", params)
   end
 
   def token_create(base_url, params) do
-    base_url
-    |> build_url("/oauth/token")
-    |> post(params)
+    conn = %MastodonClient.Conn{instance: base_url}
+    MastodonClient.post(conn, "/oauth/token", params)
   end
 
   def account_verify_credentials(base_url, token) when is_binary(token) do
-    headers = [{"Authorization", "Bearer #{token}"}]
-
-    base_url
-    |> build_url("/api/v1/accounts/verify_credentials")
-    |> get(headers: headers)
+    conn = %MastodonClient.Conn{instance: base_url, access_token: token}
+    MastodonClient.get(conn, "/api/v1/accounts/verify_credentials")
   end
 
   def account_verify_credentials(base_url, %{"access_token" => token}) when is_binary(token) do
@@ -34,11 +26,5 @@ defmodule Ueberauth.Strategy.Mastodon.API do
       query: URI.encode_query(params)
     })
     |> URI.to_string()
-  end
-
-  defp build_url(base_url, endpoint) do
-    URI.parse(base_url)
-    |> URI.merge(endpoint)
-    |> to_string()
   end
 end
